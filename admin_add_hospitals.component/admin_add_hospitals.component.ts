cf2b68e23d86b7c7c9206b251d401a17b8171021
SahithPalika <100356930+SahithPalika@users.noreply.github.com>
2022-02-27 15:58:36 +0530

    Add files via upload

---
+++ admin_add_hospitals.component/admin_add_hospitals.component.ts
@@ -0,0 +1,4 @@
+import { Component } from "@angular/core";
+
+@Component ({templateUrl:'admin_add_hospitals.component.html'})
+export class admin_add_hospitals{}
---
+++ admin_add_hospitals.component/index.ts
@@ -0,0 +1,1 @@
+export * from './admin_add_hospitals.component'
