9ea62835bd1cd429387fe79c4f4f364159ca8f38
SahithPalika <100356930+SahithPalika@users.noreply.github.com>
2022-02-27 13:07:09 +0530

    Add files via upload

---
+++ admin_login_main_page/admin_login_main_page.ts
@@ -0,0 +1,4 @@
+import { Component } from "@angular/core";
+
+@Component ({templateUrl:'admin_login_main_page.component.html'})
+export class admin_login_main_page{}
---
+++ admin_login_main_page/index.ts
@@ -0,0 +1,1 @@
+export * from './admin_login_main_page'
